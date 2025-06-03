9e1ea76065de5c37a814598cbee1074872ae3626
Elias Chen <eliaschen.dev@gmail.com>
2025-06-03 16:42:57 +0800

    add json parsing

--- app/src/main/java/com/example/composejsonobject/MainActivity.kt
+++ app/src/main/java/com/example/composejsonobject/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.composejsonobject.ui.theme.ComposejsonObjectTheme
+import org.json.JSONObject
+import kotlin.concurrent.timerTask
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +19,22 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposejsonObjectTheme {
-
+                Main()
             }
         }
+    }
+}
+
+@Composable
+fun Main() {
+    val jsonObject = JSONObject(jsonString)
+    val total = jsonObject.getInt("totalResults")
+    val resultPage = jsonObject.getJSONArray("resultPage")
+    val uri = resultPage.getJSONObject(0).getString("uri")
+
+
+    Column(modifier = Modifier.statusBarsPadding()) {
+        Text(total.toString())
+        Text(uri)
     }
 }
